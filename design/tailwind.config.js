116652b9ef24b19fa874d581c5f1f409f3b6a6f4
Shyam <shyam@Shyams-MacBook-Pro.local>
2025-06-22 16:35:26 +0530

    first commit

---
+++ design/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#f97316',         // Orange-500
+          'primary-dark': '#ea580c',  // Orange-600
+          secondary: '#3b82f6',       // Blue-500
+          'secondary-dark': '#2563eb' // Blue-600
+        },
+        fontFamily: {
+          primary: ['Poppins', 'sans-serif'],
+        }
+      }
+    }
+  }
+  
